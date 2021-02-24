5a8a6a8ab8c2008732637ce0badf7227486e3ae6
Illya Havrulyk <peoso595@gmail.com>
2021-02-24 23:52:21 +0200

    Added stylish buttons for card and dialog window while deleting item.

--- src/Components/Tiles/TileItem.jsx
+++ src/Components/Tiles/TileItem.jsx
@@ -1,30 +1,79 @@
 import { Button, Form, FormControl, Nav, Navbar, Card, CardGroup, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { NavLink } from 'react-router-dom';
-import React from 'react'
+import React from "react";
+import MaterialButton from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from "@material-ui/core/Slide";
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
 
 const Tiles = ({ goods, gridMarkup }) => {
+
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
 
-        <CardGroup style={{margin: gridMarkup === "remaining" ? " auto": null }} >
+        <CardGroup style={{ margin: gridMarkup === "remaining" ? " auto" : null }} >
             {
                 goods.map((item, index) => (
                     <Col md lg={gridMarkup === "remaining" ? true : 3} xs sm={12} key={index}>
-                        <Card key={index} style={{ width: '15rem' }}>
+                        <Card key={index} style={{ width: '15rem', marginTop: 10 + "px" }}>
                             <Card.Img variant="top" src={item.img_url} style={{ maxWidth: 25 + "%", height: "auto", margin: 0 + " auto" }} />
-                            <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
+                            <Card.Body >
+                                <Card.Title >Card Title</Card.Title>
                                 <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
+                                    {item.description.length > 60 ? item.description.slice(0, 60) + "..." : item.description}
+
                                 </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
+
+                                <Card.Text style={{ opacity: 0.8 }}>
+                                    Date added : {item.date_added}
+                                </Card.Text>
+
+                                <Button variant="success" style={{ margin: 10 + "px" }}>Edit </Button>
+                                <Button variant="danger" style={{ margin: 10 + "px" }} onClick={handleClickOpen}> Delete</Button>
                             </Card.Body>
+
+                            <Button variant="primary">View </Button>
                         </Card>
                     </Col>
-
                 ))
             }
+            <Dialog
+                open={open}
+                TransitionComponent={Transition}
+                keepMounted
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-slide-title"
+                aria-describedby="alert-dialog-slide-description"
+            >
+                <DialogTitle id="alert-dialog-slide-title">{"Use Google's location service?"}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-slide-description">
+                        Let Google help apps determine location. This means sending anonymous location data to
+                        Google, even when no apps are running.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} variant="outlined">Disagree</Button>
+                    <Button onClick={handleClose} variant="primary" color="primary" >Agree</Button>
+                </DialogActions>
+            </Dialog>
+
         </CardGroup >
     )
 }
