4f84ffaedf81bcd8628a669904a90565064b1471
Sviatoslav25 <sviatoslavvintoniv@gmail.com>
2021-02-27 22:21:38 +0200

    bug fixed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 
 import axios from "axios";
 import Header from "./Components/Header/Header";
-import { Route } from 'react-router-dom';
+import { Redirect, Route } from 'react-router-dom';
 import GoodsContainer from "./Components/Goods/GoodsContainer";
 import AddGoodsContainer from "./Components/AddGoods/AddGoodsContainer";
 import EditGoodsContainer from "./Components/EditGoods/EditGoodsContainer";
@@ -13,6 +13,9 @@ const App = () => {
   return (
     <div className="App">
       <Header />
+      <Route path="/">
+        <Redirect to="/home" />
+      </Route>
       <Route path="/home">
         <GoodsContainer />
       </Route>
