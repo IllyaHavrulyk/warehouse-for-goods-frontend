e40985c87ee9925c7d2d81de57a311511783188b
Illya Havrulyk <peoso595@gmail.com>
2021-04-05 00:50:54 +0300

    Added CORS configuration

--- src/api/api.js
+++ src/api/api.js
@@ -8,42 +8,84 @@ let instance = axios.create({
 export const goodsApi = {
 
     initialGoods() {
-        return instance.get("/product/list");
+        return axios.get("http://localhost:8080/product/list", {
+            headers: {
+                authorization: 'Basic ' + window.btoa("user123" + ":" + "user123"),
+                "Access-Control-Allow-Origin": "http://localhost:3000"
+            }
+
+        });
         //return axios.get("http://localhost:3001/Products");
     },
     putGoods(data) {
-        return instance.post("/product/save", data);
+        return instance.post("/product/save", data, {
+            headers: {
+                "Access-Control-Allow-Origin": "http://localhost:3000"
+            }
+        });
         //return axios.post("http://localhost:3001/Products", data);
     },
     deleteGoods(goodsId) {
-        return instance.delete("/product/delete/" + goodsId);
+        return instance.delete("/product/delete/" + goodsId, {
+            headers: {
+                "Access-Control-Allow-Origin": "http://localhost:3000"
+            }
+        });
         //return axios.delete("http://localhost:3001/Products/" + goodsId);
     },
     editGoods(goods) {
-        return instance.post("/product/update/" + goods.id, goods);
+        return instance.post("/product/update/" + goods.id, goods, {
+            headers: {
+                "Access-Control-Allow-Origin": "http://localhost:3000"
+            }
+        });
     },
     getGoods(goodsId) {
-        return instance.get("/product/get/" + goodsId);
+        return instance.get("/product/get/" + goodsId, {
+            headers: {
+                "Access-Control-Allow-Origin": "http://localhost:3000"
+            }
+        });
         //return axios.get("http://localhost:3001/Products/" + goodsId);
     },
     filter(minPrice, maxPrice) {
-        return instance.get(`/product/filter?minPrice=${minPrice}&maxPrice=${maxPrice}`);
+        return instance.get(`/product/filter?minPrice=${minPrice}&maxPrice=${maxPrice}`, {
+            headers: {
+                "Access-Control-Allow-Origin": "http://localhost:3000"
+            }
+        });
     },
     isLogin() {
-        return instance.get(`/login`)
+        return axios.get(`http://localhost:8080/login`, {
+            headers: {
+                "Access-Control-Allow-Origin": "http://localhost:3000"
+            }
+        })
     },
     login(userData) {
         return axios.get(`http://localhost:8080/login`,
-            { headers: { authorization: 'Basic ' + window.btoa(userData.username + ":" + userData.password) } }
+            { headers: { authorization: 'Basic ' + window.btoa(userData.username + ":" + userData.password), "Access-Control-Allow-Origin": "http://localhost:3000" } }
         );
     },
     searchGoods(goodsData) {
-        return axios.get("http://localhost:8080/product/search/" + goodsData);
+        return axios.get("http://localhost:8080/product/search/" + goodsData, {
+            headers: {
+                "Access-Control-Allow-Origin": "http://localhost:3000"
+            }
+        });
     },
     registration(login, password) {
-        return instance.post("/registration", { username: login, password });
+        return instance.post("/registration", { username: login, password }, {
+            headers: {
+                "Access-Control-Allow-Origin": "http://localhost:3000"
+            }
+        });
     },
     logout() {
-        return instance.post("/logout");
+        return instance.post("/logout", {
+            headers: {
+                "Access-Control-Allow-Origin": "http://localhost:3000"
+            }
+        });
     }
 }
